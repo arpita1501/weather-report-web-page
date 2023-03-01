e370367dc129849b18c21f7b26bc59456b0d329f
arpita1501 <114860827+arpita1501@users.noreply.github.com>
2023-03-02 00:54:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let place_name = "New Delhi";
+let key = "f4553e8da039c0d8939c85070c758d32";
+
+function func() {
+  let place = document.getElementById("searcher");
+  place_name = place.value;
+  console.log(place_name);
+  fetchWeather();
+}
+
+async function fetchWeather() {
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    place_name +
+    "&appid=" +
+    key +
+    "&units=metric";
+  const res = await fetch(url);
+  const data = await res.json();
+  const { name } = data;
+  const { temp } = data.main;
+  document.getElementById("Weatherin").innerHTML = "Right now the temperature in " + name + " is :";
+  document.getElementById("Temp").innerHTML = temp + "*C";
+}
+fetchWeather();
+
